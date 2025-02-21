a601b493dca85bb97602565f7bfd6031a136f687
NguyenHoangViet1501 <hoangvit2k4@gmail.com>
2025-02-21 18:13:27 +0700

    onlineshop

---
+++ DoAnWebNangCao/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnWebNangCao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["UserId"] == null)
+            {
+                // Nếu session chưa được thiết lập (chưa đăng nhập), chuyển hướng đến trang đăng nhập
+                return RedirectToAction("Login", "Login");
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DoAnWebNangCao/Models/ProductDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnWebNangCao.Models
+{
+    public class ProductDetailsViewModel
+    {
+
+            public Product Product { get; set; }
+            public ProductBrand ProductBrand { get; set; }
+            public ProductCatalogue ProductCatalogue { get; set; }
+        }
+    }
